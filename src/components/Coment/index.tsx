0c938181699409f0f565125c97226bcb793d5642
matheus souza <ruivorobado@gmail.com>
2021-09-19 12:30:06 -0300

    componente de comentarios corrijido

--- src/components/Coment/index.tsx
+++ src/components/Coment/index.tsx
@@ -7,15 +7,11 @@ interface PropsComent {
   comentario?: string
 }
 
-/* <div className="coment">
-  
-
-</div> */
 
 export const ComentImage: React.FC<PropsComent> = ({ img,name, comentario})=>{
   return (
     <ComentStyled>
-      <img src={img} />
+      <img src={img} alt={`imagem do/a ${name}`}/>
       <h2>{name}</h2>
       {comentario && <p className="comentario">{comentario}</p>}
     </ComentStyled>
